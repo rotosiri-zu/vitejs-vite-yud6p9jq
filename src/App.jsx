8d16abc8a7b406fa7ba6630e33b5b37a88fcf753
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-09 17:42:43 +0900

    Templateの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
 
 const user = {
   name: "ひろき",
@@ -20,13 +23,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
 export default App;
